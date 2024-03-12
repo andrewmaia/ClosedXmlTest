ad284f44a6d51554c1dfe931477ed1c94fa6ca52
Andrew Santos Maia <andrew.maia@alperseguros.com.br>
2024-03-12 17:15:05 -0300

    ajustes

---
+++ BeneficioSaudeTest.cs
@@ -0,0 +1,171 @@
+using ClosedXML.Excel;
+using ClosedXmlTest;
+using DocumentFormat.OpenXml.InkML;
+
+namespace ClosedXmlTest
+{
+    record Estrategia(string Operadora, string Plano,string ReembolsoConsulta, string Elegibilidade, string Vidas,decimal ValorPerCaptita, decimal Coparticipacao);
+    record Pessoa(string Empresa, string CNPJ, string Sexo, string Identificacao, DateTime DataNascimento, int Idade, string FaixaEtaria, string Parentesto, string Situacao, string CID, string Municipio, string UF, string Operadora, string Plano, int ValorAtual);
+    record SubEstipulante(string RazaoSocial, string CNPJ);    
+ 
+    public class BeneficioSaudeTest
+    {
+        
+        const string templateAddress = @"C:\Users\andrew.maia\Desktop\QAR\QarSaude_01_Dinamico.xlsx"; 
+        const string outputFileAddress = @"C:\Users\andrew.maia\Desktop\QAR\QarSaude_03_Preenchido.xlsx";       
+        const string 
+            consultor = "B7", 
+            razaoSocialEstipulante="B8",
+            estipulanteCnpj="J8",
+            operadoraAtual="B10",
+            adaptadoLei="D10",
+            tempoContrato="G10",
+            agregados="I10",
+            agregadosComplemento="J10",
+            aniversarioContrato="B11",
+            breakEven="D11",
+            afastados="I11",
+            afastadosComplemento="J11",
+            possuiMulta="B12",
+            regraMulta="D12",
+            aposentadosPor="I12",    
+            aposentadosPorComplemento="J12",    
+            relatorioSinistralidade="B13",
+            percentualSinistralidade="D13",
+            gestantes="I13",
+            gestantesComplemento="J13",    
+            modalidadeContrato="B14",
+            haDependentes="D14",
+            cronicos="I14",    
+            cronicosComplemento="J14",
+            haReeembolso="B15",    
+            remidos="I15",    
+            remidosComplemento="J15",    
+            coparticipacao="B16",
+            regraCoparticacao="D16",
+            inativos="I16",
+            inativosComplemento="J16",
+            regraUpDownGrade="B17",
+            prestadorServico="I17",
+            prestadorServicoComplemento="J17",
+            contribuicaoTitular="B18",    
+            contribuicaoTitularEmpresa="E18",        
+            estagiarios="I18",
+            estagiariosComplemento="J18",
+            contribuicaoDependente="B19",    
+            contribuicaoDependenteEmpresa="E19",        
+            homeCare="I19",
+            homeCareComplemento="J19",
+            elegibilidade="B20",    
+            elegibilidadeCnpj="J20",
+            estrategiaCellRange="A22:J30";
+
+        const int estrategiaReferenceLine = 31;
+    
+        public static void GenerateExcelFile(){
+ 
+            var workbook = new XLWorkbook(templateAddress);
+            var wsEstrategia = workbook.Worksheets.First(x=>x.Name=="ESTRATEGIA");
+
+            wsEstrategia.Cell(consultor).SetValue("José da Silva");
+            wsEstrategia.Cell(razaoSocialEstipulante).SetValue("Razao social Ficticia");
+            wsEstrategia.Cell(estipulanteCnpj ).SetValue("91.786.878/0001-50");
+            wsEstrategia.Cell(operadoraAtual ).SetValue("Bradesco");
+            wsEstrategia.Cell(adaptadoLei ).SetValue("Sim");
+            wsEstrategia.Cell(tempoContrato ).SetValue("2 anos");
+            wsEstrategia.Cell(agregados ).SetValue("Sim");
+            wsEstrategia.Cell(agregadosComplemento ).SetValue("Complemento");
+            wsEstrategia.Cell(aniversarioContrato).SetValue(new DateTime(2025,01,31));
+            wsEstrategia.Cell(breakEven ).SetValue("Break Even");
+            wsEstrategia.Cell(afastados ).SetValue("Não");
+            wsEstrategia.Cell(afastadosComplemento ).SetValue("Complemento");
+            wsEstrategia.Cell(possuiMulta ).SetValue("Sim");
+            wsEstrategia.Cell(regraMulta ).SetValue("Alguma Regra");
+            wsEstrategia.Cell(aposentadosPor ).SetValue("Sim");
+            wsEstrategia.Cell(aposentadosPorComplemento ).SetValue("Complemento");
+            wsEstrategia.Cell(relatorioSinistralidade ).SetValue("Sim");
+            wsEstrategia.Cell(percentualSinistralidade).SetValue( 0.2d);
+            wsEstrategia.Cell(gestantes ).SetValue("Sim");
+            wsEstrategia.Cell(gestantesComplemento ).SetValue("Complemento");
+            wsEstrategia.Cell(modalidadeContrato ).SetValue("Compulsório");
+            wsEstrategia.Cell(haDependentes ).SetValue("Sim");
+            wsEstrategia.Cell(cronicos ).SetValue("Não");
+            wsEstrategia.Cell(cronicosComplemento ).SetValue("Complemento");
+            wsEstrategia.Cell(haReeembolso ).SetValue("Sim");
+            wsEstrategia.Cell(remidos ).SetValue("Não");
+            wsEstrategia.Cell(remidosComplemento ).SetValue("Complemento");
+            wsEstrategia.Cell(coparticipacao ).SetValue("Sim");
+            wsEstrategia.Cell(regraCoparticacao ).SetValue("Alguma Regra");
+            wsEstrategia.Cell(inativos ).SetValue("Sim");
+            wsEstrategia.Cell( inativosComplemento).SetValue("Complementos");
+            wsEstrategia.Cell( regraUpDownGrade).SetValue("Alguma regra");
+            wsEstrategia.Cell( prestadorServico).SetValue("Não");
+            wsEstrategia.Cell( prestadorServicoComplemento).SetValue("Complemento");
+            wsEstrategia.Cell(contribuicaoTitular).SetValue(0.1);
+            wsEstrategia.Cell(contribuicaoTitularEmpresa).SetValue( 0.9);
+            wsEstrategia.Cell( estagiarios).SetValue("Sim");
+            wsEstrategia.Cell( estagiariosComplemento).SetValue("Complemento");
+            wsEstrategia.Cell(contribuicaoDependente).SetValue(0.3);
+            wsEstrategia.Cell(contribuicaoDependenteEmpresa).SetValue(0.7);
+            wsEstrategia.Cell( homeCare).SetValue("Sim");
+            wsEstrategia.Cell( homeCareComplemento).SetValue("Complemento");
+            wsEstrategia.Cell( elegibilidade).SetValue("Sim");
+            wsEstrategia.Cell( elegibilidadeCnpj).SetValue("91.786.878/0001-50");
+
+            List<Estrategia> estrategias = MockEstrategias();
+            IXLRange estrategiaBlock = wsEstrategia.Range(estrategiaCellRange);
+            int referenceLine=estrategiaReferenceLine;
+            foreach(var estrategia in estrategias){
+                estrategiaBlock.CopyTo(wsEstrategia.Cell(referenceLine,"A"));
+                referenceLine+=2;
+                wsEstrategia.Cell(referenceLine++,"B").SetValue(estrategia.Operadora);
+                wsEstrategia.Cell(referenceLine++,"B").SetValue(estrategia.Plano); 
+                wsEstrategia.Cell(referenceLine++,"B").SetValue(estrategia.ReembolsoConsulta); 
+                wsEstrategia.Cell(referenceLine++,"B").SetValue(estrategia.Elegibilidade);
+                wsEstrategia.Cell(referenceLine++,"B").SetValue(estrategia.Vidas); 
+                wsEstrategia.Cell(referenceLine++,"B").SetValue(estrategia.ValorPerCaptita);
+                wsEstrategia.Cell(referenceLine++,"B").SetValue(estrategia.Coparticipacao);
+                //referenceLine++;                                                                                                                        
+            }
+            estrategiaBlock.Delete(XLShiftDeletedCells.ShiftCellsUp);
+            //ABA BASE DE DADOS  ESTUDOS
+            var wsBaseDados = workbook.Worksheets.First(x=>x.Name=="BASE DE DADOS  ESTUDOS");
+            wsBaseDados.Cell("A2").InsertData(MockBaseDadosSaude());
+
+            workbook.SaveAs(outputFileAddress);
+        }
+        static List<Estrategia> MockEstrategias(){
+            return
+            [
+                 new ("Operadora", "Plano A", "Sim","Elegibilidade","Vidas",10,0.2M)
+                ,new ("Operadora", "Plano B", "Sim","Elegibilidade","Vidas",10,0.15M)
+                ,new ("Operadora", "Plano C", "Sim","Elegibilidade","Vidas",10,0.3M)
+            ];
+        }          
+
+         static List<Pessoa> MockBaseDadosSaude(){
+            return
+            [
+                new("Empresa do Joao", "41.646.207/0001-15", "Masculino", "Identificacao", new DateTime(2000, 1, 1), 24, "Faixa", "Pai", "Situacao", "11111111", "Santos", "SP", "Bradesco", "Plano Bradesco", 500),
+                new("Empresa da Maria", "41.646.207/0001-15", "Feminino", "Identificacao", new DateTime(2000, 1, 1), 24, "Faixa", "Pai", "Situacao", "11111111", "Santos", "SP", "Bradesco", "Plano Bradesco", 500),
+                new("Empresa do Roberto", "41.646.207/0001-15", "Masculino", "Identificacao", new DateTime(2000, 1, 1), 24, "Faixa", "Pai", "Situacao", "11111111", "Santos", "SP", "Bradesco", "Plano Bradesco", 500),
+                new("Empresa do José", "41.646.207/0001-15", "Feminino", "Identificacao", new DateTime(2000, 1, 1), 24, "Faixa", "Pai", "Situacao", "11111111", "Santos", "SP", "Bradesco", "Plano Bradesco", 500), 
+            ];
+ 
+        }
+
+        static List<SubEstipulante> MockSubEstimulantes(){
+            return
+            [
+                new("Empresa do Joao", "41.646.207/0001-15")
+                ,new("Empresa da Maria", "41.646.207/0001-14")
+                ,new("Empresa do José", "41.646.207/0001-16")
+            ];
+        }           
+        
+    }
+
+
+
+    
+}
